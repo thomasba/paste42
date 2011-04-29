40836cfe66820eec6db739a89023d46495899097
Thomas <git@thomasba.de>
2011-04-29 10:10:26 +0200

    modified:   paste42.sh

--- paste42.sh
+++ paste42.sh
@@ -28,7 +28,7 @@ filetypes() {
 }
 
 checkUpdate() {
-	SERV_VER=$(curl -s -S http://paste42.de/version.txt | sed -e 's/[^0-9\-]//g')
+	SERV_VER=$(curl -s -S 'http://git.thomasba.de/cgit/paste42/plain/paste42.sh' | sed -e '/VERSION=.*/ !d;s/VERSION="\(.*\)"/\1/')
 	if [ "$SERV_VER" != "$VERSION" ] ; then
 		echo "Update available!"
 		echo "Your version:   $VERSION"
